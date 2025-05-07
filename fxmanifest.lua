3011a6a6e7dbdeeb0aeabb7bb2ded2fc45d4a664
MCX Development <shefanaludnicata@gmail.com>
2025-05-07 17:55:05 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'MCX Development'
+description 'Loading screen'
+version '1.0.0'
+
+loadscreen 'index.html'
+
+files {
+    'index.html',
+    'style.css',
+    'script.js',
+    'music.mp3'
+}
+
+loadscreen_cursor 'yes'
+
+loadscreen_manual_shutdown 'yes'
